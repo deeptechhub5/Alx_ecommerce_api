1d16d9f5e9f6af9dc8e17e67856ba7a3f38b4482
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-12-18 18:51:47 +0000

    feat: fixed errors with wsgi.py for deployment

--- ecommerce_api/ecommerce_api/wsgi.py
+++ ecommerce_api/ecommerce_api/wsgi.py
@@ -1,14 +1,19 @@
-import os
-import sys
+"""
+WSGI config for ecommerce_api project.
 
-path = '/home/YOUR_USERNAME/Alx_ecommerce_api'
-if path not in sys.path:
-    sys.path.append(path)
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
     'ecommerce_api.settings'
 )
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
